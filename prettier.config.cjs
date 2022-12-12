2c388522dcffdc5a714ff05fd96532b81c57011e
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-12-12 14:59:06 -0300

    chore(code style): configurados eslint e Prettier
    Correções no eslint e adiciondas regras ao prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  arrowParens: 'avoid',
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: "lf",
+  singleAttributePerLine: true
+}
---
+++ src/App.ts
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+function App(): ReactNode {
+  return;
+}
+
+export { App };
