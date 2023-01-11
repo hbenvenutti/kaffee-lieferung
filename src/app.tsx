696f61c9984bb11ede266a906bf1f65b27fbd338
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-11 13:40:36 -0300

    fix(app theme): corrigida a implementação do contexto de temas
    O tema estava fechando em si mesmo, não passando as propriedades para os componentes.

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,15 @@ import { ThemeProvider } from 'styled-components';
 
 import { Router } from './router';
 import { defaultTheme } from './styles/themes/default-theme';
+import { GlobalStyle } from './styles';
 
 import type { ReactElement } from './@types';
 
 // ---------------------------------------------------------------------------------------------- //
 
 export const App = (): ReactElement => (
-  <>
-    <ThemeProvider theme={defaultTheme} />
+  <ThemeProvider theme={defaultTheme}>
+    <GlobalStyle />
     <Router />
-  </>
+  </ThemeProvider>
 );
