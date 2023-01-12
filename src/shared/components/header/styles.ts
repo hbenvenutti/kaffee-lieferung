f4b2ba96d38771bc12e0239d1c4c7611369969d4
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-12 01:50:23 -0300

    feat(header): estilização da div de notificação
    Feita a estilização da div que mostra a quantidade de itens no carrinho.

--- src/shared/components/header/styles.ts
+++ src/shared/components/header/styles.ts
@@ -1,6 +1,7 @@
 import { MapPin, ShoppingCart } from 'phosphor-react';
 import styled from 'styled-components';
 
+import type { CounterProps, Visibility } from './@types';
 import type { Color } from '../../../@types/styles';
 
 // ---------------------------------------------------------------------------------------------- //
@@ -9,12 +10,19 @@ export const Container = styled.header`
   display: flex;
   justify-content: space-between;
 
-  padding: 2rem 10rem;
+  padding: 2rem 0;
 
   div {
     display: flex;
     gap: 0.75rem;
     align-items: center;
+  }
+
+  .cart-div {
+    display: flex;
+    align-items: flex-start;
+    justify-content: left;
+    gap: 0px;
   }
 
   span {
@@ -41,6 +49,8 @@ export const Container = styled.header`
   }
 `;
 
+// ---------------------------------------------------------------------------------------------- //
+
 export const LocationSVG = styled(MapPin)`
   fill: ${(props): Color => props.theme['brand-primary-3']};
 
@@ -49,10 +59,35 @@ export const LocationSVG = styled(MapPin)`
   height: auto;
 `;
 
+// ---------------------------------------------------------------------------------------------- //
+
 export const CartSVG = styled(ShoppingCart)`
   fill: ${(props): Color => props.theme['brand-secondary-3']};
 
   background-color: transparent;
   width: 1.375rem;
   height: auto;
+`;
+
+// ---------------------------------------------------------------------------------------------- //
+
+export const Counter = styled.div<CounterProps>`
+  visibility: ${(props): Visibility => (props.show ? 'visible' : 'hidden')};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: ${(props): Color => props.theme['brand-secondary-3']};
+  color: ${(props): Color => props.theme['confirmation-button-text']};
+
+  border-radius: 50px;
+
+  font-weight: 700;
+
+  width: 1.25rem;
+  height: 1.25rem;
+
+  margin-left: -0.625rem;
+  margin-top: -0.5rem;
 `;
