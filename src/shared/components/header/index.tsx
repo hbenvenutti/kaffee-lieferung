32c1c1db860dd0731af7052478170e78d3a3edcf
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-12 01:49:18 -0300

    feat(header): criada a div de notificação no carrinho
    Criada a div que notifica a quantidade de itens no carrinho.

--- src/shared/components/header/index.tsx
+++ src/shared/components/header/index.tsx
@@ -1,24 +1,37 @@
 import logo from '../../../assets/logo.svg';
 
-import { CartSVG, Container, LocationSVG } from './styles';
+import { CartSVG, Container, Counter, LocationSVG } from './styles';
 
 import type { ReactElement } from 'react';
 
 // ---------------------------------------------------------------------------------------------- //
 
 export const Header = (): ReactElement => {
+  // *** ---- States ------------------------------------------------------------------------ *** //
+  // const [cartCounter, setCartCounter] = useState<number>(0);
+
+  // *** ---- Variables --------------------------------------------------------------------- *** //
+  const cartCounter = 0; // ? Temporary
+  const showCartCounter = cartCounter > 0 ? true : false;
+
+  // *** ---- TSX --------------------------------------------------------------------------- *** //
   return (
     <Container>
       <img src={logo} alt="" />
+
       <div>
         <span>
           <LocationSVG weight="fill" />
           Porto Alegre, RS
         </span>
 
-        <button>
-          <CartSVG weight="fill" />
-        </button>
+        <div className="cart-div">
+          <button>
+            <CartSVG weight="fill" />
+          </button>
+
+          <Counter show={showCartCounter}>{cartCounter}</Counter>
+        </div>
       </div>
     </Container>
   );
