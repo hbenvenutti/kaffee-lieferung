238236e677accf86480e96d2a612526cf8100d33
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-22 16:07:43 -0300

    fix(coffees): corrigido o nome da propriedade types na interface coffee

--- src/shared/providers/coffee/@types/index.d.ts
+++ src/shared/providers/coffee/@types/index.d.ts
@@ -4,7 +4,7 @@ import type { CoffeeType } from './enums';
 
 interface Coffee {
   title: string;
-  type: CoffeeType[];
+  types: CoffeeType[];
   description: string;
   price: number;
   imageUrl: string;
