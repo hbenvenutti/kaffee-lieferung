a84b1f71894cbcd1d10a8bd8f74899825db6e6e1
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-24 15:06:58 -0300

    feat(rotas): adicionada a checkout page as rotas

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/home';
 import { DefaultLayout } from './layouts/default-layout';
+import { Checkout } from './pages/checkout';
 
 import type { ReactElement } from './@types';
 
@@ -11,7 +12,7 @@ export const Router = (): ReactElement => (
   <Routes>
     <Route path="/" element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
-      <Route path="/checkout" />
+      <Route path="/checkout" element={<Checkout />} />
       <Route path="/done" />
     </Route>
   </Routes>
