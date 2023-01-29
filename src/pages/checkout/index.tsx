4964eef2bd10beb2cd5579437cb3eb3072328cda
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-01-29 19:15:11 -0300

    feat(checkout page): criado o card de endereço

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,26 +1,90 @@
+import { MapPinLine } from 'phosphor-react';
+
+import { Form } from './styles';
+
 import type { ReactElement } from '../../@types';
 
 // * ------------------------------------------------------------------------------------------ * //
 
 export const Checkout = (): ReactElement => {
   return (
-    <form action="">
-      <div>
-        <h2>Complete seu Pedido</h2>
-        <div>
-          <h4>Endereço de Entrega</h4>
-          <p>Informe o endereço onde deseja receber o pedido.</p>
+    <main>
+      <Form action="">
+        <div className="left-column">
+          <h2>Complete seu Pedido</h2>
+
+          <div className="address-card">
+            <div className="form-title">
+              <MapPinLine weight="regular" />
+              <div>
+                <h4>Endereço de Entrega</h4>
+                <p>Informe o endereço onde deseja receber o pedido.</p>
+              </div>
+            </div>
+
+            <div className="inputs">
+              <input
+                type="text"
+                name="cep"
+                id="cep"
+                placeholder="CEP"
+              />
+              <input
+                type="text"
+                name="street"
+                id="street"
+                placeholder="Rua"
+              />
+
+              <div className="input-row">
+                <input
+                  type="text"
+                  name="number"
+                  id="number"
+                  placeholder="Número"
+                />
+                <input
+                  type="text"
+                  name="complement"
+                  id="complement"
+                  placeholder="Complemento"
+                />
+                <span>Opcional</span>
+              </div>
+
+              <div className="input-row">
+                <input
+                  type="text"
+                  name="neighborhood"
+                  id="neighborhood"
+                  placeholder="Bairro"
+                />
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  placeholder="Cidade"
+                />
+                <input
+                  type="text"
+                  name="state"
+                  id="state"
+                  placeholder="UF"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="payment-method-card">
+            <h4>Pagamento</h4>
+            <p>O pagamento é feito na entrega. Escolha a forma como deseja pagar.</p>
+          </div>
         </div>
 
-        <div>
-          <h4>Pagamento</h4>
-          <p>O pagamento é feito na entrega. Escolha a forma como deseja pagar.</p>
+        <div className="right-column">
+          <h2>Cafés Selecionados</h2>
         </div>
-      </div>
-
-      <div>
-        <h2>Cafés Selecionados</h2>
-      </div>
-    </form>
+      </Form>
+    </main>
   );
 };
