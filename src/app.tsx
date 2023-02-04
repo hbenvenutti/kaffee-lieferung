98957e3c90ef5324f2bba6b9040bcdd25d2912a8
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-04 16:54:13 -0300

    feat(app): adicionado o contexto de cart por volta da aplicação

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { Router } from './router';
 import { defaultTheme } from './styles/themes/default-theme';
 import { GlobalStyle } from './styles';
+import { CartProvider } from './shared/hooks/cart-context';
 
 import type { ReactElement } from './@types';
 
@@ -10,7 +11,9 @@ import type { ReactElement } from './@types';
 
 export const App = (): ReactElement => (
   <ThemeProvider theme={defaultTheme}>
-    <GlobalStyle />
-    <Router />
+    <CartProvider>
+      <GlobalStyle />
+      <Router />
+    </CartProvider>
   </ThemeProvider>
 );
