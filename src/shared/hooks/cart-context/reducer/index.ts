aedb6ce97859291471519ec4d5cc44ddcefd66a7
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-04 18:39:33 -0300

    refactor(cart reducer): removido o calculo de total de dentro do reducer

--- src/shared/hooks/cart-context/reducer/index.ts
+++ src/shared/hooks/cart-context/reducer/index.ts
@@ -1,4 +1,4 @@
-import { calculateItemTotalPrice } from './utils';
+// import { calculateItemTotalPrice } from './utils';
 import { actionCases } from './action-cases';
 
 import type { Action, Cart } from '../@types';
@@ -7,8 +7,6 @@ import type { Action, Cart } from '../@types';
 
 export const cartItemsReducer = (state: Cart, action: Action): Cart => {
   const { item } = action.payload;
-
-  item.total = calculateItemTotalPrice(item);
 
   return actionCases[action.type](state, item);
 };
