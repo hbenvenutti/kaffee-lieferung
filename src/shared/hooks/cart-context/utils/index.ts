7c91fb5ca32c464636e7c4b3bd8f3992ba3520a0
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-02-05 04:13:44 -0300

    feat(utils): adicionado tipagem ao primeiro parametro
    O ultimo commit teve a mensagem errada, foi criada uma função para o intl

--- src/shared/hooks/cart-context/utils/index.ts
+++ src/shared/hooks/cart-context/utils/index.ts
@@ -1,7 +1,8 @@
+import type { CartItem } from '../@types';
 import type { Coffee } from '../../../providers/coffee/@types';
 
 // * ------------------------------------------------------------------------------------------ * //
 
-export const calculateItemTotalPrice = (coffee: Coffee, quantity: number): number => {
+export const calculateItemTotalPrice = (coffee: Coffee | CartItem, quantity: number): number => {
   return coffee.price * quantity;
 };
